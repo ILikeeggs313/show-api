7597820b15b7f211d6d418cfc4e00914e7f8c7fb
anhnguyen <kyanhnguyen0502@gmail.com>
2022-07-29 02:18:08 -0400

    tvshow maze api

---
+++ tvmaze.js
@@ -0,0 +1,156 @@
+/** Given a query string, return array of matching shows:
+ *     { id, name, summary, episodesUrl }
+ */
+const MISSING_IMAGE_URL = "http://tinyurl.com/missing-tv";
+
+/** Search Shows
+ *    - given a search term, search for tv shows that
+ *      match that query.  The function is async show it
+ *       will be returning a promise.
+ *
+ *   - Returns an array of objects. Each object should include
+ *     following show information:
+ *    {
+        id: <show id>,
+        name: <show name>,
+        summary: <show summary>,
+        image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
+      }
+ */
+async function searchShows(query) {
+  // TODO: Make an ajax request to the searchShows api.  Remove
+  // hard coded data.
+  const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+  let shows = res.data.map(function(result){
+    let show = result.show;
+  
+  return {
+    id : show.id,
+    name: show.name,
+    summary: show.summary,
+    image: show.image ? show.image.original: MISSING_IMAGE_URL
+  };
+  });
+  return shows;   
+}
+
+
+
+/** Populate shows list:
+ *     - given list of shows, add shows to DOM
+ */
+
+function populateShows(shows) {
+  const $showsList = $("#shows-list");
+  $showsList.empty();
+
+  for (let show of shows) {
+    let $item = $(
+      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+         <div class="card" data-show-id="${show.id}">
+      
+         <img class="card-img-top" src="${show.image}">
+           <div class="card-body">
+             <h5 class="card-title">${show.name}</h5>
+             <p class="card-text">${show.summary}</p>
+             <button id = "button">episodes </button>
+           </div>
+         </div>
+       </div>
+      `);
+//add show images using ${show.image} in the .card div
+    $showsList.append($item);
+  }
+}
+
+
+/** Handle search form submission:
+ *    - hide episodes area
+ *    - get list of matching shows and show in shows list
+ */
+
+$("#search-form").on("submit", async function handleSearch (evt) {
+  evt.preventDefault();
+
+  let query = $("#search-query").val();
+  if (!query) return;
+
+  $("#episodes-area").hide();
+
+  let shows = await searchShows(query);
+
+  populateShows(shows);
+});
+
+
+/** Given a show ID, return list of episodes:
+ *      { id, name, season, number }
+ */
+
+async function getEpisodes(id) {
+  const res = await axios.get(`http://api.tvmaze.com/shows/{${id}/episodes`);
+  let episodes = res.data.map(result => ({
+
+      id: result.id,
+      name: result.name,
+      season: result.season,
+      number: result.number,
+    
+
+  }));
+  return result;
+  // TODO: get episodes from tvmaze
+  //       you can get this by making GET request to
+  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+
+  // TODO: return array-of-episode-info, as described in docstring above
+}
+
+async function populateEpisodes(info){
+  const $episodesList = $('#episodes-list');
+  $episodesList.empty();
+  for(let episode of info){
+    let $item = $(
+      `<li>
+      ${episode.name}
+      (season ${episode.season}, episode ${episode.number}) 
+      </li>
+    `);
+    $episodesList.append($item);
+  }
+  $('#episodes-area').show();
+}
+const epiBtn = document.querySelector('#button');
+$('#show-list').on('click', '.get-episodes', async function(e){
+  let showId = $(e.target).closest('.Show').data('show-id');
+  let episodes = await getEpisodes(showId);
+  populateEpisodes(episodes);
+})
+
+async function showCast(cast){
+  const res = await axios.get(`https://api.tvmaze.com/shows/${show.id}/${cast}`);
+  let showCast = res.data.map(result => ({
+    id: result.id,
+    name: result.name,
+    cast: result.cast,
+  }));
+  return result;
+
+}
+
+
+
+// const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+//   let shows = res.data.map(function(result){
+//     let show = result.show;
+  
+//   return {
+//     id : show.id,
+//     name: show.name,
+//     summary: show.summary,
+//     image: show.image ? show.image.original: MISSING_IMAGE_URL
+//   };
+//   });
+//   return shows;   
+// }
+
